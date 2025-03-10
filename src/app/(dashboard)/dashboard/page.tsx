21175e1ade90b53c7a2f67e47903f70f72c8e0a0
jill.chang <jillchang05@gmail.com>
2025-03-09 23:09:33 -0400

    feat: add bubble

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -4,11 +4,13 @@ import Activity from "@/components/activity"
 import Assets from "@/components/assets"
 import WalletCard from "@/components/wallet-card"
 import UserTagCard from "@/components/user-tag-card"
+import BubbleComponent from "@/components/bubble"
 
 export default function Dashboard() {
   return (
     <main className="container mx-auto space-y-4 p-2 sm:p-8 lg:space-y-8 xl:px-12 2xl:px-24">
       <TransactionsProvider>
+        <BubbleComponent />
         I'm home
       </TransactionsProvider>
     </main>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata, Viewport } from "next"
 import { Inter } from "next/font/google"
 import { Providers } from "@/providers"
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
@@ -72,7 +73,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
           inter.className
         )}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          <AntdRegistry>{children}</AntdRegistry>
+        </Providers>
       </body>
     </html>
   )
